8dd247f44ec0b325751d23421ac937abc932254a
msabic <mijo.sabic@student.fsr.ba>
2017-01-25 23:38:47 +0100

    mijo
    mijo

--- dodaj.php
+++ dodaj.php
@@ -1,6 +1,22 @@
 <?php include'header.php';?>
 <link rel="stylesheet" href="assets/style.css"/>
 <script src="assets/upload.js"></script>
+
+ <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
 <!-- banner -->
 <div class="inside-banner">
   <div class="container"> 
@@ -8,6 +24,50 @@
     <h2>Dodaj kuću</h2>
 </div>
 </div>
+
+ <script>
+      // Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        var infoWindow = new google.maps.InfoWindow({map: map});
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD1v9GwfVESZsR6czrUvVVn2DjiWyVclRg&callback=initMap">
+    </script>
 <!-- banner -->
 
 
@@ -26,7 +86,8 @@
                 <label><input type="radio" name="bazen"> Bazen </label>
                 <textarea rows="6" class="form-control" placeholder="Opišite kuću" name="form_message"></textarea>
                 
- 
+ <div id="map"></div>
+
   Odaberite slike: <input type="file" name="img" multiple>
 
       <button type="submit" class="btn btn-success" name="Submit">Register</button></form>'?>
