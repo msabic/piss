f365a90d7a32e7e210ad0c7f6e6ff2f2f233f0d7
msabic <mijo.sabic@student.fsr.ba>
2017-01-28 01:23:58 +0100

    Baza Vlasnik Nekretnine
    Baza Vlasnik Nekretnine

--- dodaj_kucu.php
+++ dodaj_kucu.php
@@ -21,14 +21,25 @@ $br_kvadrata=isset($_POST["br_soba"])? $_POST["br_soba"]:"";
 $bazen=isset($_POST["bazen"])? $_POST["bazen"]:"";
 $form_message=isset($_POST["form_message"])? $_POST["form_message"]:"";
 $img=$_FILES["img"]["size"]? addslashes(file_get_contents($_FILES["img"]["tmp_name"])) : NULL;
-
-
+$id_korisnika=0;
+$id_nekretnina=0;
+$email=$_COOKIE['uname'];
+$qiii="SELECT * FROM korisnik WHERE email='$email'";
+$result=$con->query($qiii);
+while ($rez=$result->fetch_array()){
+	$id_korisnika=$rez['idkorisnik'];
+	}
 
 $qi = "Insert into nekretnina(naziv, adresa, grad, kvadratura, cijena, broj_katova, broj_soba, opis, slika) VALUE ('$naziv', '$adresa' ,'$grad', '$kvadratura', '$cijena', '$br_katova', '$br_kvadrata', '$form_message', '$img')";
-
-
-
 print $con->query($qi) ? "<script>alert('Uspješno ste se dodali kuću!')</script>" : "<script>alert('Greska')</script>";
+$qiiii="SELECT * FROM nekretnina WHERE naziv='$naziv'";
+$result=$con->query($qiiii);
+while ($rez=$result->fetch_array()){
+	$id_nekretnina=$rez['idnekretnina'];
+	}
+
+$qii="Insert into vlasnik_nekretnine( idnekretnina, korisnik_idkorisnik, korisnik_prijava_idprijava) VALUE ( '$id_nekretnina','$id_korisnika','$id_korisnika')";
+$con->query($qii) ;
 
 echo" <script type=\"text/javascript\">function closeWindow() {
     setTimeout(function() {
