f2d4de08d68bc731f059f141aabc5d98ca038ede
darkostojic <darko.stojic.93@gmail.com>
2017-01-31 00:31:13 +0100

    kalendar je dodan
    Dodan je kalendar

--- rezerviraj.php
+++ rezerviraj.php
@@ -1,15 +1,4 @@
 
-<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-  <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-
-  <script>
-
-  $(document).ready(function() {
-    $("#datepicker").datepicker();
-  });
-
-  </script>
 
 
 <?php include'header.php';?>
@@ -17,6 +6,7 @@
   <div class="container"> 
     <span class="pull-right"><a href="localhost/piss">Home</a> / Register</span>
     <h2>Rezerviraj kuću</h2>
+
 </div>
 </div>
 
@@ -24,13 +14,21 @@
 <div class="container">
 <div class="spacer">
 <div class="row register">
+<?php
 
-  <form action="sample.php" method="post">
 
-Start Date: <input type="text" name="startdate" id="datepicker"/>
-End Date: <input type="text" name="enddate" id="datepicker"/>
-<input type="submit" />
-</form>
+
+echo'
+  <div id="kalendar">
+      <script type="text/javascript">
+        $("#kalendar").multiDatesPicker({
+        maxPicks: 2,
+        minDate: 0
+        });
+      </script>
+      
+  </div>';
+?>
 
 </div>
 </div>
